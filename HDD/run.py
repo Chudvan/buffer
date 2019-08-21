fbec24c656c457dfe3b728571fce67fe0a54092a
Chudnovets Ivan <Mr.Chudvan@yandex.ru>
2019-08-21 13:38:58 +0300

    Changes_21.08.19_while_/dev/sda

--- HDD/run.py
+++ HDD/run.py
@@ -14,7 +14,7 @@ def check_args():
 
 
 def parse_disk_list():
-    list_disks = os.popen('fdisk -l | grep Disk\ /').read()
+    list_disks = os.popen('fdisk -l | grep \ /dev/').read()
     if not list_disks:
         print('Warning: empty list_disks.')
         print('Continue? (y/n)')
@@ -24,29 +24,31 @@ def parse_disk_list():
     return [string.split()[-1] for string in list_disks.split(':')[:-1]]
 
 
+def get_life_time(hdd):
+    return int(os.popen('smartctl -a "{}" | grep "# 1" \
+    | tr -s " " | cut -f9 -d" "'.format(hdd)).read())
+
+
 def run_smartctl(list_disks, TIME_SLEEP):
-    test_disks = []
-    for disk in list_disks:
-        result = int(os.popen('smartctl -a /dev/sda | grep "# 1" | tr -s " " | cut -f9 -d" "').read())
-        os.popen('smartctl -t short "{}"'.format(disk))
-        time.sleep(TIME_SLEEP)
-        result -= int(os.popen('smartctl -a /dev/sda | grep "# 1" | tr -s " " | cut -f9 -d" "').read())
-        if bool(result):
-            test_disks.append(disk)
-    return test_disks
+    for hdd in list_disks:
+        cur_life_time = get_life_time(hdd)
+        start_time = time.time()
+        os.popen('smartctl -t short "{}"'.format(hdd))
+        while cur_life_time == get_life_time(hdd) and (time.time() - start_time) <= TIME_SLEEP:
+            time.sleep(5)
 
 
 def delete_entries(DELETE_UNTIL):
     os.system('python "{}" "{}"'.format('delete_entry.py', time.time() - DELETE_UNTIL))
 
 
-def new_tests(result_disks):
-    for disk in result_disks:
+def new_tests(list_disks):
+    for disk in list_disks:
         os.system('python "{}" "{}"'.format('HDD.py', disk))
 
 
 check_args()
 list_disks = parse_disk_list()
-result_disks = run_smartctl(list_disks, TIME_SLEEP)
+run_smartctl(list_disks, TIME_SLEEP)
 delete_entries(DELETE_UNTIL)
-new_tests(result_disks)
+new_tests(list_disks)
